c35717d72e428cb4f95f543c09c347f383b59cca
pangtuwi <pangtuwi@gmail.com>
2016-07-16 12:02:10 +0100

    Initial Commit

---
+++ wwJobs/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  wwJobs
+//
+//  Created by Paul Williams on 16/07/2016.
+//  Copyright © 2016 pwilliams. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
